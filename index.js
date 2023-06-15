2d0a56f0036dd7d7ca2f397b0892a7edaae6200c
sasha wangui <sasha.iluku@gmail.com>
2023-06-15 03:23:28 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// const divide = x => 2000/100;
+
+function divide (x){
+    return 2000/100
+}
+
+const square = x => {
+    return x ** x
+}
+console.log(2)
+
+const add = (a, b) => {
+    return a + b
+}
+console.log(3, 4)
